ca43721dea5e7ed5c562c21621c3cbe9ac143884
David Castro Castaneda <dcastrocast@gmail.com>
2020-11-22 21:48:12 -0800

    UI vars added to calcRes func

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ document
 // Calculate Results
 function calculateResults(e) {
   console.log('calculating');
+  // UI Variables
+  const amount = document.getElementByID('amount');
+  const interest = document.getElementByID('interest');
+  const years = document.getElementByID('years');
+  const monthlyPayment = document.getElementByID('monthly-payment');
+  const totalPayment = document.getElementByID('total-payment');
+  const totalInterest = document.getElementByID('total-interest');
 
   e.preventDefault();
 }
