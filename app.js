dff03b91f805957fe79c18b90c7aa2cef4342939
David Castro Castaneda <dcastrocast@gmail.com>
2020-11-22 21:06:58 -0800

    func CalcRes

---
+++ app.js
@@ -0,0 +1,11 @@
+// Listen for submit
+document
+  .getElementById('loan-form')
+  .addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+  console.log('calculating');
+
+  e.preventDefault();
+}
